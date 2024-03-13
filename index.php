f86ca0c9e1db11d2b8f48cca589614cb82aefa81
MedHK008 <mohammedherak37@gmail.Com>
2024-03-13 10:55:34 +0000

    update

--- index.php
+++ index.php
@@ -9,17 +9,25 @@
 </head>
 <body>
     <div id="welcome">
-        <h1>Bienvenue</h1>
-        <div>
-        <form action="insertion.php" method="post">
-            <button class="button-89" role="button">Ajouter un étudiant</button>
-        </form>
-        </div>
-        <div>
-        <form action="recherche.php" method="post">
-            <button class="button-89" role="button">Rechercher un étudiant</button>
-        </form>
-        </div>
+        <table>
+            <tr>
+                <td><h1>Bienvenue</h1></td>
+            </tr>
+            <tr>
+                <td>
+            <form action="insertion.php" method="post">
+                <button class="button-89" role="button">Ajouter un étudiant</button>
+            </form>
+                </td>
+            </tr>
+            <tr>
+                <td>
+            <form action="recherche.php" method="post">
+                <button class="button-89" role="button">Rechercher un étudiant</button>
+            </form>
+                </td>
+            </tr>
+        </table>
     </div>
 </body>
 </html>
